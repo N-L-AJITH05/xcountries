6da5800983da62a51b7519cd700501cc2c337563
N-L-AJITH05 <ajithram3999@gmail.com>
2024-07-10 18:44:20 +0530

    second commit

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,57 +1,38 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Countries.css";
 
 const Tile = ({ flagUrl, name, altFlag }) => {
     return (
-        <div className="countryCard">
+        <div className="tile">
             <img src={flagUrl} alt={altFlag} className="flag-image" />
             <h2>{name}</h2>
         </div>
     );
 };
 
-
 function Countries() {
-  const API_URL = "https://restcountries.com/v3.1/all";
-  const [countries, setCountries] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
+    const API_URL = "https://xcountries-backend.azurewebsites.net/all"; 
+    const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => setCountries(data))
-      .catch((error) => console.error("Encountered error: ", error));
-  }, []);
+    useEffect(() => {
+        fetch(API_URL)
+            .then((response) => response.json())
+            .then((data) => setCountries(data))
+            .catch((error) => console.error("Error fetching data: ", error));
+    }, []);
 
- 
-  const filteredCountries = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  console.log({ countries });
-
-  return (
-    <div className="container">
-      <input
-        className="Search"
-        type="text"
-        placeholder="Search for countries"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <div className="countries">
-        {filteredCountries.map((country) => (
-          <Tile
-            key={country.cca2}
-            flagUrl={country.flags.png}
-            name={country.name.common}
-            altFlag={country.flags.alt}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="countries">
+            {countries.map((country) => (
+                <Tile 
+                    key={country.abbr} 
+                    flagUrl={country.flag} 
+                    name={country.name} 
+                    altFlag={country.flag.alt} 
+                />
+            ))}
+        </div>
+    );
 }
 
 export default Countries;
